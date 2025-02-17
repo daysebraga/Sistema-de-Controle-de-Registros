bf5e43c25f426e1bc4b1634771bc732760d35e31
Dayse Braga <daysebraga2803@gmail.com>
2025-02-17 00:00:44 -0300

    Update ReciboDeInstrucoes.java

--- ReciboDeInstrucoes.java
+++ ReciboDeInstrucoes.java
@@ -3,6 +3,15 @@ public class ReciboDeInstrucoes {
 
     void enviaInstrucao(){
 
+        System.out.println("Escreva a mensagem desejada: ");
+        String msg[] =  scanner.nextLine();
+        System.out.println("digite o ID do funcionario");
+        String Id = scanner.nextLine();
+
+        for(int i=0; i<funcionario.length; i++){
+            if(funcionario.id == Id){
+            }
+        }
     }
 
 }
